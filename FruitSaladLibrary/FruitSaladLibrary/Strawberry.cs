d5c2cdd7a7bc7aabd55754efc66c8827da061eef
Mystery <koby.bradley@icloud.com>
2025-01-21 19:40:54 -0500

    Someone put a banana in my salad!
    An example of a strawberry in an inheritance and an abstraction

---
+++ FruitSaladLibrary/FruitSaladLibrary/Strawberry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitSaladLibrary
+{
+    public class Strawberry : Fruit
+    {
+        public bool IsItGoodForJam { get; set; }
+        public int SweetnessLevel { get; set; }
+        public Strawberry(string color, double weight, bool isOrganic, bool isItGoodForJam, int sweetnessLevel) :
+            base("Strawberry", color, weight, isOrganic)
+        {
+            SweetnessLevel = sweetnessLevel;
+            IsItGoodForJam = isItGoodForJam;
+        }
+        //implementation of the abstract Describe method
+        public override string Describe()
+        {
+            string jamLabel = IsItGoodForJam ? "Good for jam!" : "Holy mother of Joseph this sh** is nasty!";
+            return $"{base.ToString()}, Sweetness Level: {SweetnessLevel}, {jamLabel}";
+        }
+    }
+}
